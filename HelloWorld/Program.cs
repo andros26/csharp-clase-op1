ac6811bc8e3278a2f749cf83e98bd4477697e956
andros26 <108378283+andros26@users.noreply.github.com>
2025-03-17 05:38:12 -0500

    Add new code for age calculation

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,13 +1,43 @@
-﻿namespace HelloWorld {
+﻿using System.Data;
+using System.Dynamic;
 
-    class Program{
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateOnly dataConverted = new DateOnly();
+            string nameInput;
+            string birthdayInput;
+            Console.WriteLine("Calculador de años");
+            Console.Write("Escribe tu nombre: ");
+            nameInput = Console.ReadLine();
+            Console.Write("Ingresa tu fecha de nacimiento en formato dd/mm/yy: ");
+            birthdayInput = Console.ReadLine();
+            bool isDataValid = DateOnly.TryParse(birthdayInput, out dataConverted);
+            if (isDataValid == false) Console.WriteLine($"La fecha {birthdayInput} es invalida");
+            var person = new Person
+            {
+                Name = nameInput,
+                Birthday = dataConverted,
+                Age = DateTime.Now.Year - dataConverted.Year
+            };
 
-        static void Main(){
-            
-            var Name = "Jhon";
-            Console.WriteLine($"Hello world {Name}");
-            Console.WriteLine();
+            Console.WriteLine($"Tú nombre: {person.Name}");
+            Console.WriteLine($"Tú fecha de nacimiento:  {person.Birthday}");
+            Console.WriteLine($"Tú edad es:  {person.Age} años");
+
+            Console.ReadLine();
 
         }
     }
+}
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public DateOnly Birthday { get; set; }
+
 }
